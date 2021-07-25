0c0598b33f9b5141b96734b49de477a947e06fd0
2sweetheart2 <2004sweetheart2004@gmail.com>
2021-07-25 15:33:54 +0300

    the plugin operation has been changed

--- src/main/java/me/sweettie/Handler.java
+++ src/main/java/me/sweettie/Handler.java
@@ -5,48 +5,45 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class Handler implements Listener {
 
     public static List<String> dangerousWords = new ArrayList<>();
     public static Boolean randomPlayer = true;
+    public static int radius;
 
     @EventHandler
-    public void chat(PlayerChatEvent event){
-        if(event.getPlayer().getWorld().equals(Bukkit.getWorlds().get(0))) {
-            String message = event.getMessage().toLowerCase();
-            for (int i = 0; i < dangerousWords.size(); i++) {
-                if (message.contains(dangerousWords.get(i).toLowerCase())) {
-                    if (randomPlayer) {
-                        Random random = new Random();
-                        ArrayList<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
-                        int index = random.nextInt(players.size());
-                        Player target = players.get(index);
-                        Location playerLocation = event.getPlayer().getLocation();
-                        Location location = new Location(event.getPlayer().getWorld(), playerLocation.getBlockX() + getRandomInt(-10.0, 10.0), playerLocation.getBlockY(), playerLocation.getBlockZ() + getRandomInt(-10.0, 10.0));
-                        createStrikeLight(location,target);
-                    }
-                    else{
-                        Player target = event.getPlayer();
-                        Location playerLocation = event.getPlayer().getLocation();
-                        Location location = new Location(event.getPlayer().getWorld(), playerLocation.getBlockX() + getRandomInt(-10.0, 10.0), playerLocation.getBlockY(), playerLocation.getBlockZ() + getRandomInt(-10.0, 10.0));
-                        createStrikeLight(location,target);
-                    }
-                }
+    public void chat(AsyncPlayerChatEvent event) {
+        if (event.getPlayer().getWorld() != Bukkit.getWorlds().get(0)) return;
+        String message = event.getMessage().toLowerCase();
+        for (String s : dangerousWords) {
+            if (!message.contains(s)) return;
+            Player player;
+            if (randomPlayer) {
+                Random random = new Random();
+                player = Bukkit.getWorlds().get(0).getPlayers().get(random.nextInt(Bukkit.getWorlds().get(0).getPlayers().size()));
+            } else {
+                player = event.getPlayer();
             }
+            Location loc = player.getLocation();
+            loc.setY(player.getWorld().getHighestBlockYAt(loc.getBlockX(), loc.getBlockZ()));
+            createStrikeLight(loc);
         }
     }
-    public static double getRandomInt(double min, double max){
-        double x = (int)(Math.random()*((max-min)+1))+min;
-        return x;
+
+    public static double getRandomInt(double min, double max) {
+        return (int) (Math.random() * ((max - min) + 1)) + min;
     }
-    public void createStrikeLight(Location loc,Player target){
-        target.getPlayer().getWorld().strikeLightningEffect(loc);
+
+    public void createStrikeLight(Location loc) {
+        Location location = new Location(loc.getWorld(), loc.getBlockX() + getRandomInt(-radius, radius), loc.getY(), loc.getBlockZ() + getRandomInt(-radius, radius));
+        Objects.requireNonNull(loc.getWorld()).strikeLightningEffect(location);
     }
 
 
--- src/main/java/me/sweettie/Main.java
+++ src/main/java/me/sweettie/Main.java
@@ -6,26 +6,25 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin {
 
-    public static Logger log = Logger.getLogger("Minecraft");
     private List<String> words = new ArrayList<>();
     private Boolean randomPlayer = true;
+    public int radius;
 
     @Override
     public void onEnable() {
         getLogger().info("ENABLE!");
-        File config = new File(getDataFolder() + File.separator+"config.yml");
-        if(!config.exists()){
+        File config = new File(getDataFolder() + File.separator + "config.yml");
+        if (!config.exists()) {
             getConfig().options().copyDefaults(true);
             saveDefaultConfig();
         }
-        Bukkit.getPluginManager().registerEvents(new Handler(),this);
+        Bukkit.getPluginManager().registerEvents(new Handler(), this);
         words = getConfig().getStringList("dangerousWords");
         randomPlayer = getConfig().getBoolean("randomPlayer");
-
+        radius = getConfig().getInt("radius");
         setDangerousWords();
     }
 
@@ -34,8 +33,9 @@ public final class Main extends JavaPlugin {
         getLogger().info("DISABLE!");
     }
 
-    private void setDangerousWords(){
+    private void setDangerousWords() {
         Handler.dangerousWords = words;
         Handler.randomPlayer = randomPlayer;
+        Handler.radius = radius;
     }
 }
